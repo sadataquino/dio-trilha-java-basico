c737c66949abb0d5074b13360d99ec832f509501
sadataquino <sadataquino@gmail.com>
2022-10-26 13:48:47 -0300

    Tipos de variaveis

--- tipo-variaveis/src/TiposVariaveis.java
+++ tipo-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,17 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        int num$int = 1;
+        double num$double = 2.5;
+        String nome = "Pessoa";
+        boolean decisao = true;
+        char letra = 'M';
+
+
+        System.out.println(num$int);
+        System.out.println(num$double);
+        System.out.println(nome);
+        System.out.println(decisao);
+        System.out.println(letra);
     }
 }
